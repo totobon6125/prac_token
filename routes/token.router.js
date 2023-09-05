e26ad257c7852cd509a649fad31914a649b96620
totobon6125 <totobon6125@gmail.com>
2023-09-05 13:41:25 +0900

    "end"

--- routes/token.router.js
+++ routes/token.router.js
@@ -2,7 +2,7 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
-dotenv.config(); 
+dotenv.config();
 
 const router = express.Router();
 
@@ -16,12 +16,12 @@ const tokenStorages = {}
 // AK, RK 발급 API
 router.post('', (req, res) => {
     // ID 전달
-    const {id} = req.body;
+    const { id } = req.body;
 
     // AT, RT 발급
-    const accessToken = jwt.sign({id:id}, ACCESS_TOKEN_SECRET_KEY, {expiresIn: '10s'});
-    const refreshToken = jwt.sign({id:id}, REFRESH_TOKEN_SECRET_KEY, {expiresIn: '7d'});
-       
+    const accessToken = createAccessToken(id);
+    const refreshToken = jwt.sign({ id: id }, REFRESH_TOKEN_SECRET_KEY, { expiresIn: '7d' });
+
     // RT 발급받으면 상태정보가 저장됨.
     tokenStorages[refreshToken] = {
         id: id,
@@ -32,7 +32,64 @@ router.post('', (req, res) => {
     res.cookie('accessToken', accessToken);
     res.cookie('refreshToken', refreshToken);
 
-    return res.status(200).json({msg: 'Token 발급 완료!'});
+    return res.status(200).json({ msg: 'Token 발급 완료!' });
+});
+
+// AT 검증 API
+router.get('/validate', async (req, res) => {
+    const { accessToken } = req.cookies;
+
+    // AT가 존재하는지 확인
+    if (!accessToken) {
+        return res.status(400).json({ errMsg: 'AT가 존재하지 않습니다.' });
+    }
+
+    const payload = ValidateToken(accessToken, ACCESS_TOKEN_SECRET_KEY)
+
+    if (!payload) {
+        return res.status(400).json({ errMsg: 'AT이 정상적이지 않습니다.' });
+    }
+
+    const { id } = payload;
+    return res.status(200).json({ msg: `${id}의 payload를 가진 Token이 정상적으로 인증 되었습니다.` });
+
+})
+
+// Token을 검증하고, payLoad 를 조회하기 위한 함수
+function ValidateToken(token, secretKey) {
+    try {
+        return jwt.verify(token, secretKey);
+    } catch (err) {
+        return null;
+    }
+}
+
+function createAccessToken(id) {
+    return jwt.sign({id}, ACCESS_TOKEN_SECRET_KEY, {expiresIn: '10s'})
+}
+
+// RT를 이용해서 AT를 재발급 하는 API
+router.post('/refresh', async (req, res) => {
+    const { refreshToken } = req.cookies;
+
+    if (!refreshToken) {
+        return res.status(400).json({ errMsg: 'RT가 존재하지 않습니다.' })
+    }
+    
+    const payload = ValidateToken(refreshToken, REFRESH_TOKEN_SECRET_KEY);
+    if (!payload) {
+        return res.status(401).json({ errMsg: 'RT가 정상적이지 않습니다' });
+    }
+
+    const userInfo = tokenStorages[refreshToken];
+    if(!userInfo) {
+        return res.status(419).json({errMsg: 'RT가 서버에 존재하지 않습니다.'});
+    }
+
+    const newAccessToken = createAccessToken(userInfo.Id)
+
+    res.cookie('accessToken', newAccessToken);
+    return res.status(200).json({msg:'AT를 정상적으로 새롭게 발급했습니다.'})
 });
 
 export default router;
